ec8d0d2cca500cdd8bad4479558f57a9243ec3fb
HristoAngeloff <66922355+Iccooo@users.noreply.github.com>
2023-06-22 14:56:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,38 +5,62 @@ import SH1107
 import time
 import config
 import traceback
-
+import obd
+from obd import OBDStatus
 from PIL import Image,ImageDraw,ImageFont
 
 try:
     disp = SH1107.SH1107()
-
-    print("1.3inch OLED ")
     disp.Init()
-    rmp = 0
-    speed = 1
+    
+    rpm = 0
+    speed = 0
+    
+    connection = obd.OBD("/dev/ttyUSB0")
     while(1):
-        print ("working on output")
-        image1 = Image.new('1', (disp.width, disp.height), "WHITE")
-        draw = ImageDraw.Draw(image1)
+        if(connection.status() == OBDStatus.CAR_CONNECTED):
+            print ("working on output")
+            cmd_speed = obd.commands.SPEED
+            response_speed = connection.query(cmd)
 
-        font_text = ImageFont.truetype('Font.ttf', 13)
-        draw.text((19,0), 'RMP', font = font_text, fill = 0)
-        draw.arc((0,17,64,100),0, 360, fill =0 )
-        draw.text((18, 43), str(rmp), font= font_text, fill=0)
+            cmd_rpm = obd.commands.SPEED
+            response_rpm = connection.query(cmd)
+            image1 = Image.new('1', (disp.width, disp.height), "WHITE")
+            draw = ImageDraw.Draw(image1)
 
-        draw.text((76, 0), 'Speed', font = font_text, fill = 0)
-        draw.arc((64,17,127,100),0, 360, fill =0 )
-        draw.text((89, 43), str(speed), font = font_text, fill = 0)
+            font_text = ImageFont.truetype('Font.ttf', 13)
+            draw.text((19,0), 'RPM', font = font_text, fill = 0)
+            draw.arc((0,17,64,100),0, 360, fill =0 )
+            draw.text((18, 43), str(response_rpm.value), font= font_text, fill=0)
 
-        disp.clear()
-        #image1=image1.rotate(180)
-        print("new output")
-        disp.ShowImage(disp.getbuffer(image1))
-        rmp+=100
-        speed+=3
-        print("Test rmp " + str(rmp) + " Test speed" + str(speed))
-        time.sleep(0.5)
+            draw.text((76, 0), 'Speed', font = font_text, fill = 0)
+            draw.arc((64,17,127,100),0, 360, fill =0 )
+            draw.text((89, 43), str(response_speed.value), font = font_text, fill = 0)
+
+            disp.clear()
+            image1=image1.rotate(180)
+            print("new output")
+            disp.ShowImage(disp.getbuffer(image1))
+            print("Rmp " + str(rpm) + " Speed" + str(speed))
+            time.sleep(0.5)
+        else:
+            image1 = Image.new('1', (disp.width, disp.height), "WHITE")
+            draw = ImageDraw.Draw(image1)
+
+            font_text = ImageFont.truetype('Font.ttf', 13)
+            draw.text((1,0), 'CAR NOT CONNECTED', font = font_text, fill = 0)
+            draw.arc((0,17,64,100),0, 360, fill =0 )
+            draw.text((18, 43), str(rpm), font= font_text, fill=0)
+
+            draw.arc((64,17,127,100),0, 360, fill =0 )
+            draw.text((89, 43), str(speed), font = font_text, fill = 0)
+
+            disp.clear()
+            image1=image1.rotate(180)
+            print("new output")
+            disp.ShowImage(disp.getbuffer(image1))
+            print("Rmp " + str(rpm) + " Speed" + str(speed))
+            time.sleep(0.5)
 
 except IOError as e:
     print(e)
